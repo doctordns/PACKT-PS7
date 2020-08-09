b0576acb1427ae68af2d76c78e1e81fd8ad5cc35
Thomas Lee <DoctorDNS@Gmail.Com>
2020-08-09 17:50:35 +0100

    ch 2 iniitial creation

---
+++ scripts/Ch 2 - Install-Config Pwsh/2.2 - Install-VSCode.ps1
@@ -0,0 +1,118 @@
+﻿# 2.2 Install-VSCode
+# 
+# Run on DC1 after installing PowerShell 7
+# Run in PowerShell 7 console
+
+# 1. Download the VS Code Installation Script
+$VSCPATH = 'C:\Foo'
+Save-Script -Name Install-VSCode -Path $VSCPATH
+Set-Location -Path $VSCPATH
+
+# 2. Now run it and add in some popular VSCode Extensions
+$Extensions =  "Streetsidesoftware.code-spell-checker",
+               "yzhang.markdown-all-in-one"
+$InstallHT = @{
+  BuildEdition         = 'Stable-System'
+  AdditionalExtensions = $Extensions
+  LaunchWhenDone       = $true
+}             
+.\Install-VSCode.ps1 @InstallHT 
+
+#  At this point, VS Code should be displayed.
+#  The remainder of this script in VS Code 
+#  MAKE SURE YOU RUN VSCode as Admin
+
+# 3. Create a Sample Profile File
+$SAMPLE = 'https://raw.githubusercontent.com/doctordns/Wiley20/master/' +
+          'Goodies/Microsoft.VSCode_profile.ps1'
+(Invoke-WebRequest -Uri $Sample).Content |
+  Out-File $Profile
+
+
+# 4. Download Cascadia Code font from GitHub
+# Get File Locations
+$CascadiaFont    = 'Cascadia.ttf'    # font file name
+$CascadiaRelURL  = 'https://github.com/microsoft/cascadia-code/releases'
+$CascadiaRelease = Invoke-WebRequest -Uri $CascadiaRelURL # Get all of them
+$CascadiaPath    = "https://github.com" + ($CascadiaRelease.Links.href | 
+                      Where-Object { $_ -match "($CascadiaFont)" } | 
+                        Select-Object -First 1)
+$CascadiaFile   = "C:\Foo\$CascadiaFont"
+# Download Cascadia Code font file
+Invoke-WebRequest -Uri $CascadiaPath -OutFile $CascadiaFile
+
+# 5. Install Cascadia Code font
+$FontShellApp = New-Object -Com Shell.Application
+$FontShellNamespace = $FontShellApp.Namespace(0x14)
+$FontShellNamespace.CopyHere($CascadiaFile, 0x10)
+
+# 6. Update Local User Settings for VS Code
+#    This step in particular needs to be run in PowerShell 7!
+$JSON = @'
+{
+  "workbench.colorTheme": "PowerShell ISE",
+  "powershell.codeFormatting.useCorrectCasing": true,
+  "files.autoSave": "onWindowChange",
+  "files.defaultLanguage": "powershell",
+  "editor.fontFamily": "'Cascadia Code',Consolas,'Courier New'",
+  "workbench.editor.highlightModifiedTabs": true,
+  "window.zoomLevel": 1
+}
+'@
+$JHT = ConvertFrom-Json -InputObject $JSON -AsHashtable
+$PWSH = "C:\\Program Files\\PowerShell\\7\\pwsh.exe"
+$JHT += @{
+  "terminal.integrated.shell.windows" = "$PWSH"
+}
+$Path = $Env:APPDATA
+$CP   = '\Code\User\Settings.json'
+$Settings = Join-Path  $Path -ChildPath $CP
+$JHT |
+  ConvertTo-Json  |
+    Out-File -FilePath $Settings
+
+# 7. Create a short cut to VSCode
+$SourceFileLocation  = "$env:ProgramFiles\Microsoft VS Code\Code.exe"
+$ShortcutLocation    = "C:\foo\vscode.lnk"
+# Create a  new wscript.shell object
+$WScriptShell        = New-Object -ComObject WScript.Shell
+$Shortcut            = $WScriptShell.CreateShortcut($ShortcutLocation)
+$Shortcut.TargetPath = $SourceFileLocation
+#Save the Shortcut to the TargetPath
+$Shortcut.Save()
+
+# 8. Create a short cut to PowerShell 7
+$SourceFileLocation  = "$env:ProgramFiles\PowerShell\7\pwsh.exe"
+$ShortcutLocation    = 'C:\Foo\pwsh.lnk'
+# Create a  new wscript.shell object
+$WScriptShell        = New-Object -ComObject WScript.Shell
+$Shortcut            = $WScriptShell.CreateShortcut($ShortcutLocation)
+$Shortcut.TargetPath = $SourceFileLocation
+#Save the Shortcut to the TargetPath
+$Shortcut.Save()
+
+# 9. Build Updated Layout XML
+$XML = @'
+<?xml version="1.0" encoding="utf-8"?>
+<LayoutModificationTemplate
+  xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+  xmlns:defaultlayout=
+    "http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+  xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+  xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+  Version="1">
+<CustomTaskbarLayoutCollection>
+<defaultlayout:TaskbarLayout>
+<taskbar:TaskbarPinList>
+ <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Foo\vscode.lnk" />
+ <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Foo\pwsh.lnk" />
+</taskbar:TaskbarPinList>
+</defaultlayout:TaskbarLayout>
+</CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>
+'@
+$XML | Out-File -FilePath C:\Foo\Layout.Xml
+
+# 10. Import the  start layout XML file
+#     You get an error if this is not run in an elevated session
+Import-StartLayout -LayoutPath C:\Foo\Layout.Xml -MountPath C:\
---
+++ scripts/Ch 2 - Install-Config Pwsh/2.3 - Using PowerShell Gallery.ps1
@@ -0,0 +1,20 @@
+# 2.3 - Using the PowerShell Gallery
+# 
+# Run on DC1  after installing PowerShell 7 and VS Code
+
+# 1. Get Details of all PS Gallery Modules
+$PGSM = Find-Module -Name *
+"There are {0:N0} modules in the PS Gallery" -f $PGSM.Count
+
+# 2. Get Details of packages tagged with 'PSEdition_Core'
+$PGSMC = Find-Module -Name * -Tag 'PSEdition_Core'
+"There are {0:N0} modules supporting PowerShell Core" -f $PGSMC.Count
+
+# 3. Find NTFS Modules
+$PGSM | Where-Object Name -match 'NTFS'
+
+# 4. Install the NTFSSecurity Module
+Install-Module -Name NTFSSecurity -Force -Scope 'CurrentUser'
+
+# 5. View Commands in the NTFS Security module
+Get-Command -Module NTFSSecurity
